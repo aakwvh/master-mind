ee2b393dc5e8402f7c5a5631345ffc74bb3a00b4
aakwvh <agus.vanhuut@student.hu.nl>
2022-04-05 19:04:19 +0200

    test git oefenen

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
-# This is a sample Python script.
+from itertools import permutations
+import random
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 # algoritm
+# stap 1, zoek een random nummer in de range van 1000 en 10000
+code = random.randrange(1000,10000) # dit zorgt voor dat er een random een nummer tussen 1000 en 10000 wordt gekozen.
+print("dit is de code", code)
 
-# stap 1 maak een liojst met alle mogelijke combinatie/gok de lijst is gesorteerd.
 
-# stap 2  stel een vraag: doe een gok?
+# stap 2 maak een lijst met alle mogelijke combinatie/gok de lijst is gesorteerd.
+# bij deze stap wordt alle mogelijke  combinatie in een list toegevoegd en gesorteerd van klein naar groot.
+alleCombinatie = [] # maakt een lege list
+for i in range(1000,10000):
+    alleCombinatie.append(i) # er wordt geloop van 1000 tot 10000, alles loops wordt toegevoegd aan de list
+sorted_alleCombinatie = sorted(alleCombinatie) # de lijst wordt gesorteerd van klein naar groot.
 
-# stap 3
+# stap 3 stel een vraag: doe een gok?
+guess = random.choice(sorted_alleCombinatie)
+print("dit is guess",guess)
+
+# stap 4
+
+# Python program to print all
+# the possible combinations
+
+
+# Get all combination of [A, 2, 3]
+# of length 3
+
+
+
+
+alle_combi = []
+
+
+#
+# combinatie = permutations(['rood','groen','paars','oranje'], 4)
+# for i in combinatie:
+#     print(i)
+#     alle_combi.append(i)
+#
+# print(sorted(alle_combi))
+#
+# vraag = 'wat is de eerste combinatie?'
+# vraag_input = input("voeg hier input?")
+
+

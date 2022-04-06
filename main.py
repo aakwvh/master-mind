0e85d8c458774eb07399e993935c720e8d96dbcc
aakwvh <agus.vanhuut@student.hu.nl>
2022-04-06 15:48:46 +0200

    ik heb code geschreven waarbij wordt gecheck of een value van guess in code plaats vind, en de waarde die in code zitten wordt in een nieuwe list heet correct list. met deze correct list wordt er neiuwe alle mogelijke combi gegeven maar dan met conditie als de combi de value bevatten van variabvel correct.

--- main.py
+++ main.py
@@ -2,11 +2,49 @@ from itertools import permutations
 import random
 
 
+# deze functie maakt to split an integer into an array of digits, vorbeeld 1240 naar [1,2,4,0]
+def array_digit(getal):
+    return [int(i) for i in str(getal)]
+
+# deze functie verandert list arrey naar int.
+def magic(numList):
+    s = ''.join(map(str, numList))
+    return int(s)
+
+
+# in deze stap wordt gecheck welke waarde van de guess in de juist code zitten.
+a = [1,2,4,5]
+b = [2,1,5,9]
+
+c = []
+for i in b:
+    if i in a:
+        print(i)
+        c.append(i)
+
+print(c)
+
+numbers_to_find = str(magic(c))
+print(numbers_to_find)
+
+lst = [str(i) for i in range(1000,10000)]
+
+possible_code = []
+for i in lst:
+    if numbers_to_find in i:
+        possible_code.append(i)
+print(possible_code)
+
+k = array_digit(int(random.choice(possible_code)))
+print(k)
+
+
+
 # algoritm
 # stap 1, zoek een random nummer in de range van 1000 en 10000
 code_ = random.randrange(1000,10000) # dit zorgt voor dat er een random een nummer tussen 1000 en 10000 wordt gekozen.
 
-code = map(int,str(code_))
+code = array_digit(code_)
 print("dit is de code", code)
 
 # stap 2 maak een lijst met alle mogelijke combinatie/gok de lijst is gesorteerd.
@@ -16,8 +54,8 @@ for i in range(1000,10000):
     alleCombinatie.append(i) # er wordt geloop van 1000 tot 10000, alles loops wordt toegevoegd aan de list
 sorted_alleCombinatie = sorted(alleCombinatie) # de lijst wordt gesorteerd van klein naar groot.
 
-# stap 3 stel een vraag: doe een gok?
-guess = random.choice(sorted_alleCombinatie)
+# stap 3 doe een gok?
+guess = array_digit(random.choice(sorted_alleCombinatie))
 print("dit is guess",guess)
 
 
@@ -43,6 +81,8 @@ def right_inwrongplace(guess, code):
 print(right_inwrongplace(guess, code))
 # Python program to print all
 # the possible combinations
+
+
 
 
 # Get all combination of [A, 2, 3]
